5414fa6538bf1cb2a4936872a65dc3eba564e42e
shreya <shreya.sharad.jadhav@gmail.com>
2019-03-26 07:27:19 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'users/new'
+
+  get  '/help', to: 'static_pages#help'
+  get '/about', to:'static_pages#about'
+  get '/contact', to:'static_pages#contact'
+  get '/signup', to: 'users#new'
   root 'static_pages#home'
 end
